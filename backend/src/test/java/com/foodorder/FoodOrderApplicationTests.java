e1ec5c37041d143a5cef2db11f5e658d68ba8f23
ZHENG Xuqian <24024019D@connect.polyu.hk>
2026-04-30 02:12:47 +0800

    Add initial test class for FoodOrder application

---
+++ backend/src/test/java/com/foodorder/FoodOrderApplicationTests.java
@@ -0,0 +1,12 @@
+package com.foodorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
